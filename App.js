752aee50b8ce5d4ecffa0ce4b1e19d7457436256
Tymee Kong <tymeekong562@gmail.com>
2023-10-27 01:27:34 -0700

    Added icons to the navigator

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
+import Ionicons from '@expo/vector-icons/Ionicons';
 import Home from './Screens/Home';
 import Login from './Screens/Login';
 import Profile from './Screens/Profile';
@@ -9,7 +10,35 @@ const Tab = createBottomTabNavigator();
 export default function App() {
     return (
         <NavigationContainer>
-            <Tab.Navigator>
+            <Tab.Navigator
+                screenOptions={({ route }) => ({
+                    tabBarIcon: ({ focused, color, size }) => {
+                        let iconName;
+                        if (route.name === 'Home') {
+                            iconName = focused
+                                ? 'ios-information-circle'
+                                : 'ios-information-circle-outline';
+                        } else if (route.name === 'Login') {
+                            iconName = focused
+                                ? 'ios-log-in'
+                                : 'ios-log-in-outline';
+                        } else if (route.name === 'Profile') {
+                            iconName = focused
+                                ? 'ios-person'
+                                : 'ios-person-outline';
+                        }
+                        return (
+                            <Ionicons
+                                name={iconName}
+                                size={size}
+                                color={color}
+                            />
+                        );
+                    },
+                    tabBarActiveTintColor: 'tomato',
+                    tabBarInactiveTintColor: 'gray',
+                })}
+            >
                 <Tab.Screen name='Home' component={Home} />
                 <Tab.Screen name='Login' component={Login} />
                 <Tab.Screen name='Profile' component={Profile} />
